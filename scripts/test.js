3d3f66fea62ea9487cfbe2f589efab633f92463a
Chris McKenzie <crmckenzie@gmail.com>
2017-05-11 12:06:25 -0700

    Got karma working

--- scripts/test.js
+++ scripts/test.js
@@ -1,21 +1,37 @@
 'use strict';
 
 process.env.NODE_ENV = 'test';
-process.env.PUBLIC_URL = '';
+process.env.BABEL_ENV = 'development';
 
 // Load environment variables from .env file. Suppress warnings using silent
 // if this file is missing. dotenv will never modify any environment variables
 // that have already been set.
 // https://github.com/motdotla/dotenv
 require('dotenv').config({silent: true});
+const path = require('path');
+const Server = require('karma').Server;
+var config = path.resolve('config/karma.conf.js');
 
-const jest = require('jest');
-const argv = process.argv.slice(2);
+var singleRun = false;
+var autoWatch = true;
 
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
+console.log(process.argv);
+
+if (process.argv.indexOf("--singleRun") > -1) {
+  console.log("turning on singlerun, and turning autowatch off: " + process.argv)
+  singleRun = true;
+  autoWatch = false;
 }
 
+var settings = {configFile: config, singleRun: singleRun, autoWatch: autoWatch};
 
-jest.run(argv);
+var options = require(config);
+options.singleRun = singleRun;
+options.autoWatch = autoWatch;
+
+var server = new Server(settings, function(exitCode) {
+  console.log('Karma has exited with ' + exitCode)
+  process.exit(exitCode)
+});
+
+server.start();
