a7fc3f2a6f0d704030020f94baf738dad7ef6a70
Chris McKenzie <crmckenzie@gmail.com>
2017-05-22 11:58:50 -0700

    added missing dev dependencies

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,25 +1,12 @@
-import Page from 'react-page-object'
 import React from 'react'
 import App from 'App'
 import { shallow } from 'enzyme';
 
 describe('AppSpec', function() {
-  let page
-
-  beforeEach(function() {
-    page = new Page(<App />)
-  })
-
-  afterEach(function() {
-    page.destroy()
-  })
-
-  it('should pass', function() {
-    expect(page.content()).to.match(/Welcome to React/)
-  })
 
   it('supports enzyme', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("div.App")).to.have.length(1);
   });
+
 })
